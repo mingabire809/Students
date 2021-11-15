d18a30590afac71a82cd533d6db9df3e4845f59f
Michel Ingabire <ingabiremichel@gmail.com>
2021-11-16 01:10:01 +0300

    Initial commit

---
+++ src/main/java/com/example/Students/student/StudentConfig.java
@@ -0,0 +1,39 @@
+package com.example.Students.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student mariam = new Student(
+
+                    "Mariam",
+                    "mariam.jama@gmail.com",
+                    LocalDate.of(2000, AUGUST, 5)
+
+            );
+            Student alex = new Student(
+
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2000, AUGUST, 6)
+
+            );
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+        };
+    }
+}
